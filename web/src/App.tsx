577995889087de40103db149a2f17e6fbc65d699
Uriel Fuentes <1039215@homedepot.com.mx>
2021-09-17 22:55:13 -0500

    commit

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import logo from "./logo.svg";
 import { Counter } from "./features/counter/Counter";
+import { Box, Container, Flex } from "@chakra-ui/react";
 
 function App() {
-  return <></>;
+  return (
+    <Flex
+      h="100vh"
+      direction={["column", null, null, "row"]}
+      pt={[20, null, null, null, 40]}
+    >
+      <Box flex={["0", null, null, "1"]} bg="tomato" />
+      <Box flex={["1", null, null, "2"]} bg="brown"></Box>
+      <Box flex={["1", null, null, "1"]} bg="twitter.400" />
+    </Flex>
+  );
 }
 
 export default App;
